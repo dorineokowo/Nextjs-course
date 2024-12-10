a115e328fb647df026e742396239fcdc05b5d2a2
dorine-okowo <dorineokowo0@gmail.com>
2024-12-10 13:55:27 +0300

    Changes

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,17 +1,19 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
-import { notFound } from 'next/navigation';
-export default async function Page(props: { params: Promise<{ id: string }> }) {
-  const params = await props.params;
-  const id = params.id;
+import { notFound } from "next/navigation";
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const { id } = params; // Destructure the ID from params
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
   if (!invoice) {
     notFound();
   }
+
   return (
     <main>
       <Breadcrumbs
